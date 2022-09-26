4fc873c6a5f75a5537cf06f01b8d7d0312b7399e
zhaoyuanmeng <1426593075@qq.com>
2022-09-26 20:49:36 +0800

    feat: 基本输出

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let a = 10;
+    let b = 20i32;
+
+    let mut c = 30_i32;
+
+    let d = add(10, 20);
+
+    print!("sum = {}", d)
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    // 这里不写return 和分号 不然就会当作一个语句了
+    a + b
 }
