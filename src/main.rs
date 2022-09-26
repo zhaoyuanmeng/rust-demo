11c450c03948b2ebdefbb3b638b585c181ce8ca4
zhaoyuanmeng <1426593075@qq.com>
2022-09-26 21:21:26 +0800

    feat: add 解构

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 fn main() {
-    let a = 10;
-    let b = 20i32;
+    let _a = 10;
+    let _b = 20i32;
 
-    let mut c = 30_i32;
+    let mut _c = 30_i32;
 
     let d = add(10, 20);
 
-    print!("sum = {}", d)
+    let (_e, _f): (bool, bool) = (true, false);
+
+    print!("sum = {}\n", d);
+    print!("e = {}\n", _e);
+    print!("f = {}\n", _f);
 }
 
 fn add(a: i32, b: i32) -> i32 {
     // 这里不写return 和分号 不然就会当作一个语句了
-    a + b
+    return a + b;
 }
