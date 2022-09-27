b8c51dcb4a8c1e9ce17cd8dab9dda4bdf456a1a0
zhaoyuanmeng <1426593075@qq.com>
2022-09-27 09:48:19 +0800

    feat: 测试使用数学库

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,36 @@
-fn main() {
-    let _a = 10;
-    let _b = 20i32;
 
-    let mut _c = 30_i32;
+// fn main() {
+//     let _a = 10;
+//     let _b = 20i32;
 
-    let d = add(10, 20);
+//     let mut _c = 30_i32;
 
-    let (_e, _f): (bool, bool) = (true, false);
+//     let d = add(10, 20);
 
-    // ..= 表示序列
-    for i in 1..=5{
-        println!("{}", i);
-    }
+//     let (_e, _f): (bool, bool) = (true, false);
 
-    print!("sum = {}\n", d);
-    print!("e = {}\n", _e);
-    print!("f = {}\n", _f);
-}
+//     // ..= 表示序列
+//     for i in 1..=5{
+//         println!("{}", i);
+//     }
 
-fn add(a: i32, b: i32) -> i32 {
-    // 这里不写return 和分号 不然就会当作一个语句了
-    return a + b;
-}
+//     print!("sum = {}\n", d);
+//     print!("e = {}\n", _e);
+//     print!("f = {}\n", _f);
+// }
+
+// fn add(a: i32, b: i32) -> i32 {
+//     // 这里不写return 和分号 不然就会当作一个语句了
+//     return a + b;
+// }
+
+use num::complex::Complex;
+
+ fn main() {
+   let a = Complex { re: 2.1, im: -1.2 };
+   let b = Complex::new(11.1, 22.2);
+   let result = a + b;
+
+   println!("{} + {}i", result.re, result.im)
+ }
+
