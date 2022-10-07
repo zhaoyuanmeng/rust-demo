29b52848019dcc984cb0b53b2b6c04a5cf4cfb0d
zhaoyuanmeng <1426593075@qq.com>
2022-10-07 19:35:51 +0800

    feat: perfeat guess_number game

--- guessing_name/src/main.rs
+++ guessing_name/src/main.rs
@@ -1,3 +1,33 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    let secreat_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is {secreat_number}!");
+    loop {
+        println!("Please input your gusess!");
+
+        let mut gusess = String::new();
+
+        io::stdin()
+            .read_line(&mut gusess)
+            .expect("Failed to read Line");
+
+        // 必须将string转化为number才能够比较
+        let gusess: u32 = match gusess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed:{gusess}");
+        // 比较部分
+        match gusess.cmp(&secreat_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
